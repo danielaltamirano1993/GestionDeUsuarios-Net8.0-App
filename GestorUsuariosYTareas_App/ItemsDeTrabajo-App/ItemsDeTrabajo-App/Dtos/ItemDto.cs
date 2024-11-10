3c5a630baddf0bd65b13c1f9ee1db6b96d7e29ec
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-10 06:50:11 -0500

    Create ItemDto.cs

---
+++ GestorUsuariosYTareas_App/ItemsDeTrabajo-App/ItemsDeTrabajo-App/Dtos/ItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ItemsDeTrabajo_App.Dtos
+{
+    public class ItemDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Prioridad { get; set; }
+
+        //public DateTime FechaEntrega { get; set; }
+        public string FechaEntrega { get; set; }
+        public string Estado { get; set; }
+    }
+
+    public class UpdateEstadoDto
+    {
+        public string NuevoEstado { get; set; }
+    }
+
+}
