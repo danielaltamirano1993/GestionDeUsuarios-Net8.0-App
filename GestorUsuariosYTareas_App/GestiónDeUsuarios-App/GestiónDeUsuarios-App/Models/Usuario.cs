aa9a7b1e32869b3c49698dda57eb3023c53c8e8e
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-10 06:51:35 -0500

    Create Usuario.cs

---
+++ GestorUsuariosYTareas_App/GestiónDeUsuarios-App/GestiónDeUsuarios-App/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionDeUsuarios_App.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public int ConteoPendientes { get; set; }
+        public int ConteoAltaPrioridad { get; set; }
+    }
+}
