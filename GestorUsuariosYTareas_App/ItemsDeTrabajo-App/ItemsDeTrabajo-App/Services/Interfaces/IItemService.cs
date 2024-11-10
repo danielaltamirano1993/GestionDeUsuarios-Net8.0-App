3241799674e8e506ab08080c9832c5d73f581369
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-10 06:50:46 -0500

    Create IItemService.cs

---
+++ GestorUsuariosYTareas_App/ItemsDeTrabajo-App/ItemsDeTrabajo-App/Services/Interfaces/IItemService.cs
@@ -0,0 +1,24 @@
+﻿using ItemsDeTrabajo_App.Models;
+using ItemsDeTrabajo_App.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GestionDeUsuarios_App.Models;
+
+namespace ItemsDeTrabajo_App.Services.Interfaces
+{
+    public interface IItemService
+    {
+        Task<ItemDto> CreateItem(ItemDto itemDto);
+        Task<IEnumerable<ItemDto>> GetAllItems();
+        Task<Item> ObtenerItemPorId(int id);
+        Task<Item> GetItemByIdAsync(int id);
+        Task UpdateItem(Item item);
+        Task<bool> DeleteItem(int id);
+        Task<bool> AsignarItemAUsuario(int itemId, int usuarioId);
+
+        DbSet<Item> Items { get; }
+        Task SaveChangesAsync();
+        Task<IQueryable<Usuario>> ObtenerUsuariosAsync();
+    }
+}
