02e65688113ae525ad93a6867925c200f72fc995
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-10 06:51:18 -0500

    Create DbContext.cs

---
+++ GestorUsuariosYTareas_App/GestiónDeUsuarios-App/GestiónDeUsuarios-App/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionDeUsuarios_App.Models;
+
+namespace GestionDeUsuarios_App.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
