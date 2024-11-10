ffd0cde88cb8dcba837f2b480957ef88720ce9dc
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-10 06:50:27 -0500

    Create Item.cs

---
+++ GestorUsuariosYTareas_App/ItemsDeTrabajo-App/ItemsDeTrabajo-App/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace ItemsDeTrabajo_App.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Prioridad { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public string Estado { get; set; }
+        public int? UsuarioAsignadoId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
