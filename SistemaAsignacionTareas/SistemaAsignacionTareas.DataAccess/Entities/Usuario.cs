a766b48ba9e364a502bd7639684e759364fd24b2
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-11 19:56:57 -0500

    Create Usuario.cs

---
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionDeTrabajo.Data.Entities
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; }
+
+        public ICollection<ItemDeTrabajo> ItemsDeTrabajoPendientes { get; set; }
+    }
+}
