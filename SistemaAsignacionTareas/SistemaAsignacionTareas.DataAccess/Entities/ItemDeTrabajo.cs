92f1a4e1d3210a42e43b84746d2a5f3216869b5c
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-11 19:56:51 -0500

    Create ItemDeTrabajo.cs

---
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Entities/ItemDeTrabajo.cs
@@ -0,0 +1,16 @@
+﻿namespace GestionDeTrabajo.Data.Entities
+{
+    public class ItemDeTrabajo
+    {
+        public int Id { get; set; }  // Clave primaria
+
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public string Relevancia { get; set; }  // "Alta" o "Baja"
+
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
