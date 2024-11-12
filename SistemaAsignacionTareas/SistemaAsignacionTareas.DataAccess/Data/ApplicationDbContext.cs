728914c11308e2c1217768af1594007761d5cd0d
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-11 19:56:38 -0500

    Create ApplicationDbContext.cs

---
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionDeTrabajo.Data.Entities;
+
+namespace GestionDeTrabajo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<ItemDeTrabajo> ItemsDeTrabajo { get; set; }
+        public DbSet<Asignacion> Asignaciones { get; set; }
+    }
+}
