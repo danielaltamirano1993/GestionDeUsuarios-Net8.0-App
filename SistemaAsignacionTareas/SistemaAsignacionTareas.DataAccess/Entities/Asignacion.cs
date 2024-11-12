80653931f8617492c5348be04f6ac880a1cd79a3
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-11 19:56:47 -0500

    Create Asignacion.cs

---
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Entities/Asignacion.cs
@@ -0,0 +1,15 @@
+﻿namespace GestionDeTrabajo.Data.Entities
+{
+    public class Asignacion
+    {
+        public int AsignacionId { get; set; }
+        public int UsuarioId { get; set; }
+        public int ItemId { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+        public string EstadoAsignacion { get; set; }  // "Pendiente" o "Completado"
+
+        // Relación con las entidades Usuario y ItemDeTrabajo
+        public Usuario Usuario { get; set; }
+        public ItemDeTrabajo ItemDeTrabajo { get; set; }
+    }
+}
