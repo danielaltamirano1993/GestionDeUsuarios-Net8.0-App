7a3c2f41e1f1d22a46620d71ee9d2e6f890829f1
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-23 17:15:41 -0500

    Update Usuario.cs

--- SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Entities/Usuario.cs
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Entities/Usuario.cs
@@ -1,10 +1,13 @@
-﻿namespace GestionDeTrabajo.Data.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace GestionDeTrabajo.Data.Entities
 {
     public class Usuario
     {
         public int UsuarioId { get; set; }
         public string NombreUsuario { get; set; }
 
+        [JsonIgnore]
         public ICollection<ItemDeTrabajo> ItemsDeTrabajoPendientes { get; set; }
     }
 }
