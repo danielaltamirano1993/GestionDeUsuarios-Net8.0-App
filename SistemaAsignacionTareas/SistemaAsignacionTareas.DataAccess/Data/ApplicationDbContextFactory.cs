dd7b559cf0bd25239e27f5ef3003ac1e94625594
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2024-11-23 17:15:50 -0500

    Update ApplicationDbContextFactory.cs

--- SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Data/ApplicationDbContextFactory.cs
+++ SistemaAsignacionTareas/SistemaAsignacionTareas.DataAccess/Data/ApplicationDbContextFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 
 public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
@@ -9,11 +10,18 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        // Obtener el entorno actual, usa "Development" como predeterminado si no está definido
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        // Configuración para cargar los archivos de configuración en función del entorno
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
             .Build();
 
+        // Obtener la cadena de conexión
         var connectionString = configuration.GetConnectionString("DefaultConnection");
         optionsBuilder.UseSqlServer(connectionString);
 
